51aed96cdf91e1263f89ec7be069016e7fe0d819
M-E-A <miles.e.allen@gmail.com>
2018-11-21 11:49:33 -0500

    Minor additions to the test file, likely misinterpreting the source code.

--- dasio/test/dasio_test.cc
+++ dasio/test/dasio_test.cc
@@ -7,8 +7,11 @@ class TO_test : public ::testing::Test {
   protected:
     void SetUp() {
       TO.Clear();
+	  socket = new socket(/*no clue*/);
     }
     Timeout TO;
+	TimeoutAccumulator TA;
+	Socket socket;
 };
 
 TEST_F(TO_test, StartsClear) {
@@ -21,6 +24,46 @@ TEST_F(TO_test, TimesOut) {
   EXPECT_FALSE(TO.Expired());
   sleep(2);
   EXPECT_TRUE(TO.Expired());
+}
+
+// Code added by Miles on 20 November 2018
+
+// This method tests that the timer Set method instantiates correctly.
+TEST_F(TO_test) {
+  EXPECT_FALSE(TO.Set());
+  TO.Set(2,0);
+  EXPECT_TRUE(TO.Set());
+  EXPECT_FALSE(TO.Expired());
+}
+
+// This method tests the functionality of the Clear method when Set.
+TEST_F(TO_test) {
+  TO.Set(2,0);
+  sleep(1);
+  EXPECT_FALSE(TO.Expired());
+  TO.Clear();
+  EXPECT_TRUE(TO.Expired());
+}
+
+// This method tests the functionality of the Set_Min method.
+TEST_F(TO_test) {
+  TO.Set(2,0);
+  TA.Set(TO);
+  TO.SET(4,0);
+  TA.Set(TO);
+  TO.Set(6,0);
+  TA.Set(TO);
+  EXPECT_FALSE(TA.timeval==2);
+  TA.Set_Min();
+  EXPECT_TRUE(TA.timeval==2);
+}
+
+// This method tests functionality of the socket
+TEST_F(TO_test) {
+  socket.connect();
+  socket.readSockError(0);
+  socket.reset();
+  socket.readSockError(0);
 }
 
 int main(int argc, char **argv) {
