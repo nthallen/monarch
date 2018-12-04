09bc65ee4c858f2d42496cc26324e90f4950c716
M-E-A <miles.e.allen@gmail.com>
2018-12-04 15:59:29 -0500

    Another commit of test_not

--- dasio/test/test_not.cc
+++ dasio/test/test_not.cc
@@ -28,6 +28,7 @@ bool not_tester::seed_buf(const char *str) {
   int length = strlen(&str);
   if (length < buf_size) {
     strncpy(DAS_IO::Interface::buf, &str, length);
+	DAS_IO::Interface::cp = 0;
     return true;
   } else {
     return false;
@@ -37,10 +38,12 @@ bool not_tester::seed_buf(const char *str) {
 /* This method tests functionality of the constructor */
 TEST(NotTest, NotTesterTest) {
   not_tester::not_tester("NotTesterInstance",8);
-  EXPECT_FALSE(not_tester::seed_buf("FarFarFarTooBig"));
-  EXPECT_FALSE(DAS_IO::Interface::cp < DAS_IO::Interface::nc);
-  EXPECT_TRUE(not_tester::seed_buf("Correct"));
-  EXPECT_TRUE(DAS_IO::Interface::cp < DAS_IO::Interface::nc);
+  const char *wrong_string = "FarFarFarTooBig";
+  const char *right_string = "Correct";
+  EXPECT_FALSE(not_tester::seed_buf(&wrong_string));
+  EXPECT_TRUE(DAS_IO::Interface::cp <= DAS_IO::Interface::nc);
+  EXPECT_TRUE(not_tester::seed_buf(&right_string));
+  EXPECT_TRUE(DAS_IO::Interface::cp == strlen(&right_string));
 }
 
 /* This method tests functionality of not_float() */
