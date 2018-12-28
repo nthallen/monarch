dea1e45880b1498de8a7d9da06c34e03eec9b0e1
Norton Allen <allen@huarp.harvard.edu>
2018-12-28 15:32:32 -0500

    Automate running multiple tests

--- dasio/test/runtests.sh
+++ dasio/test/runtests.sh
@@ -1,21 +1,31 @@
 #! /bin/bash
+builddirs=$1
+if [ -z "$builddirs" ]; then
+  srcdir=`dirname $0`
+  echo srcddir is $srcdir
+  builddirs=`ls -d $srcdir/../../build*/dasio/test`
+  echo builddirs are $builddirs
+fi
+for builddir in $builddirs; do
+  PASSED=''
+  FAILED=''
+  cd $builddir
+  for i in test_*; do
+    echo $i
+    if ./$i; then
+      PASSED="$PASSED $i"
+    else
+      FAILED="$FAILED $i"
+    fi
+  done
+  cd -
 
-PASSED=''
-FAILED=''
-for i in test_*; do
-  echo $i
-  if ./$i; then
-    PASSED="$PASSED $i"
+  echo
+  echo "Results for $builddir:"
+  if [ -n "$FAILED" ]; then
+    echo "PASSED:$PASSED"
+    echo "FAILED:$FAILED"
   else
-    FAILED="$FAILED $i"
+    echo "All tests PASSED"
   fi
 done
-
-if [ -n "$FAILED" ]; then
-  echo "PASSED:$PASSED"
-  echo "FAILED:$FAILED"
-else
-  echo "All tests PASSED"
-fi
-
-  
