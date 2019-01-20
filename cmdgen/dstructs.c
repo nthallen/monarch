7c2eb895634d31f632468d7a1e88033cf5ce89f7
Norton Allen <allen@huarp.harvard.edu>
2019-01-20 18:14:03 -0500

    cmdgen: fix includes for strcasecmp

--- cmdgen/dstructs.c
+++ cmdgen/dstructs.c
@@ -20,6 +20,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <assert.h>
 #include "cmdgen.h"
 #include "compiler.h"
--- cmdgen/interfaces.c
+++ cmdgen/interfaces.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <strings.h>
 #include "cmdgen.h"
 #include "compiler.h"
 #include "nl.h"
