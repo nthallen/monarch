97c705c9daaee8f5bd5dd90c93d21aa6b8ddeb25
M-E-A <miles.e.allen@gmail.com>
2019-01-22 16:22:11 -0500

    Added the part where we do something.

--- memo/template.cc
+++ memo/template.cc
@@ -8,6 +8,9 @@ DAS_IO::AppID_t AppID("boerf", "The boerf driver for the fuermflauz", "v1.0");
 int main(int argc, char **argv) {
   oui_init_options(argc, argv);
   msg(0, "Startup %s, %s", AppID.fullname, AppID.rev);
+  
+  //do_the_work();
+  
   msg(0, "Termination");
   return 0;
 }
