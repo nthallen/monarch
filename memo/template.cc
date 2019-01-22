29b881f8a8c025ee06e7a832266ed9756e58f7e5
M-E-A <miles.e.allen@gmail.com>
2019-01-22 16:13:52 -0500

    Added template for boerfen

---
+++ memo/template.cc
@@ -0,0 +1,13 @@
+/* A template file for a main function */
+
+#include "dasio/appid.h"
+#include "oui.h"
+
+DAS_IO::AppID_t AppID("boerf", "The boerf driver for the fuermflauz", "v1.0");
+
+int main(int argc, char **argv) {
+  oui_init_options(argc, argv);
+  msg(0, "Startup %s, %s", AppID.fullname, AppID.rev);
+  msg(0, "Termination");
+  return 0;
+}
