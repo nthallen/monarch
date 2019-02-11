c64b62aa218058ab5f35efeb880ddb3d88641c34
M-E-A <miles.e.allen@gmail.com>
2019-02-11 11:26:39 -0500

    Added test folder, much needed.

---
+++ memo/test/test_msg.cc
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <time.h>
+#include "dasio/appid.h"
+#include "oui.h"
+#include "nl.h"
+#include "test_msg.h"
+
+DAS_IO::AppID_t DAS_IO::AppID("name","fullname","version");
+
+int main(int argc, char** argv) {
+  //create a test client, connect?
+  
+  oui_init_options(argc,argv);
+  
+  
+}
