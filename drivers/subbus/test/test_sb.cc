9ba4c4260ab9bf1e0251b5472b7a83c2d7f9c4ca
Norton Allen <allen@huarp.harvard.edu>
2019-02-28 19:45:30 -0500

    CAN: Updated test to include mread (which fails)

--- drivers/subbus/test/test_sb.cc
+++ drivers/subbus/test/test_sb.cc
@@ -8,6 +8,11 @@
 
 DAS_IO::AppID_t DAS_IO::AppID("test_sb", "Subbus Test Program", "V1.0");
 
+typedef struct {
+  uint16_t n_words;
+  char name[0x40];
+} device_name_t;
+
 int main(int argc, char **argv) {
   oui_init_options(argc, argv);
   subbuspp *P = new subbuspp("subbusd", "CAN");
@@ -21,5 +26,13 @@ int main(int argc, char **argv) {
   msg(0, "Read 0x%04X from 0x0121", value);
   value = P->write_ack(0x0121, 0x55AA);
   if (!value) msg(0, "no ack at 0x0121");
+  device_name_t dname;
+  subbus_mread_req *mreq = P->pack_mread_request((sizeof(device_name_t)+1)/2, "6|40@7");
+  int rv = P->mread_subbus(mreq, (uint16_t*)&dname);
+  if (rv < 0) {
+    msg(2, "mread_subbus returned error %d", rv);
+  } else if (rv == SBS_NOACK) {
+    msg(1, "mread_subbus return SBS_NOACK");
+  }
   P->subbus_quit();
 }
