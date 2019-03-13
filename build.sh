4d0b566d082b55e48e772a7837c0474f122c742d
Norton Allen <allen@huarp.harvard.edu>
2019-03-13 15:25:23 -0400

    cmake: cleaning up dependencies

--- build.sh
+++ build.sh
@@ -7,22 +7,31 @@ function msg {
   echo "build.sh: $*" >&2
 }
 
+if [ "$1" = "cross" ]; then
+  crname='-cross'
+  crargs=" -DCMAKE_TOOLCHAIN_FILE=arm-toolchain.cmake"
+else
+  crname=''
+  crargs=''
+fi
+  
 branch=`git rev-parse --abbrev-ref HEAD`
 if [ ! -d .git ]; then
   msg Not running in the source root
-  while [ ! -d .git ]; do
+  while [ `pwd` != '/' && ! -d .git ]; do
     cd ..
   done
   [ -d .git ] || nl_error Unable to locate source root
   [ -f le-dasng-doxygen.css ] || nl_error Not in the correct source tree
 fi
-if [ ! -d build-$branch ]; then
-  msg Creating build build-$branch
-  mkdir build-$branch
-  cd build-$branch
+builddir="build$crname-$branch"
+if [ ! -d $builddir ]; then
+  msg Creating build $builddir
+  mkdir $builddir
+  cd $builddir
   cmake -DCMAKE_BUILD_TYPE=Debug .. && make
 else
-  msg Using existing build-$branch
-  cd build-$branch
+  msg Using existing $builddir
+  cd $builddir
   make
 fi
