f7a45d16552f65112c03790ce437ad561654f956
Norton Allen <allen@huarp.harvard.edu>
2019-04-15 10:36:15 -0400

    Add root.cmd, loop.cmd and flttime.tmc

---
+++ tools/util/root.cmd
@@ -0,0 +1,29 @@
+%{
+  #include <string.h>
+  #include <errno.h>
+  #include "nl.h"
+%}
+
+%INTERFACE <tm_gen:tm_gen/cmd>
+%INTERFACE <Quit>
+
+&start
+  : &commands Quit * { msg(0,"Shutting down"); }
+  : &commands &&Exit
+  ;
+&&Exit
+  : Exit * {}
+  ;
+&commands
+  :
+  : &commands &command
+  ;
+&command
+  : *
+  : Telemetry &tm_cmd
+  : Log %s ( Enter String to Log to Memo ) * {}
+  ;
+&tm_cmd
+  : Start * { if_tm_gen.Turf( "TMc\n" ); }
+  : Single Step * { if_tm_gen.Turf( "TMs\n" ); }
+  ;
