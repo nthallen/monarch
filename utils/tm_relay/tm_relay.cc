e3509e67a8290c95dd819e70fd670f10d80d5e55
miles <miles@huarp.harvard.edu>
2019-11-14 14:35:32 -0500

    tm_relay: initial commit

---
+++ utils/tm_relay/tm_relay.cc
@@ -0,0 +1,7 @@
+/** @file tm_relay.cc
+ *  To serve as middleman between two points of communication
+ *  @date 2019-11-14
+ */
+
+#include "tm_relay.h"
+
---
+++ utils/tm_relay/tm_relay.h
@@ -0,0 +1,9 @@
+#include "tm.h"
+
+public class tm_relay : public tm_client {
+  public:
+    tm_relay();
+    void connect();
+  protected:
+  private:
+}
