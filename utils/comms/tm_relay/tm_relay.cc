c113409df8af5b21bb4a2bac66bc5c9303c2cee6
miles <miles@huarp.harvard.edu>
2019-11-20 16:24:54 -0500

    tm_relay: beginnings of tm_relay functions

--- utils/comms/tm_relay/tm_relay.cc
+++ utils/comms/tm_relay/tm_relay.cc
@@ -8,7 +8,9 @@
 #include "tm_client.h"
 
 using namespace DAS_IO {
-
+  
+DAS_IO::AppID_t DAS_IO::AppID("tm_relay", "tm_relay", "V1.0");
+  
 tm_relay::tm_relay() : tm_generator(), tm_client(64, false, "tm_relay") {}
 
 void tm_relay::process_data() {
@@ -19,4 +21,16 @@ void tm_relay::process_tstamp() {
   commit_tstamp();
 }
 
+int main() {
+  tm_relay::tm_relay new_tm_relay();
+  Loop ELoop;
+  ELoop.add_child(new_tm_relay);
+  msg(0, "tm_relay starting");
+  new_tm_relay.connect();
+  ELoop.event_loop();
+  new_tm_relay.Start();
+  msg(0, "tm_relay terminating");
+  return 0;
+}
+  
 }
--- utils/comms/tm_relay/tm_relay.h
+++ utils/comms/tm_relay/tm_relay.h
@@ -7,6 +7,7 @@ using namespace DAS_IO {
 class tm_relay : public tm_generator, public tm_client {
   public:
     tm_relay();
+    ~tm_relay();
   protected:
     virtual void process_data();
     virtual void process_tstamp();
