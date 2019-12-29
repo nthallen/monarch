f3f4c0ff628efa9c1ede1139b4daed76b26d1db6
Norton Allen <allen@huarp.harvard.edu>
2019-12-29 15:07:25 -0500

    timeout.h: improve documentation

--- libs/dasio/src/dasio/timeout.h
+++ libs/dasio/src/dasio/timeout.h
@@ -11,17 +11,26 @@ namespace DAS_IO {
 class Timeout {
   public:
     Timeout();
-    // Timeout( time_t secs, long msecs );
+    /**
+     * Specifies a desired timeout relative to the current time. The event loop
+     * will subtract the then-current time to determine the correct relative
+     * timeout value.
+     * @param secs Seconds
+     * @param msecs Milleseconds
+     */
     void Set( time_t secs, long msecs );
+    /**
+     * Clears any current timeout setting.
+     */
     void Clear();
-  /**
-   * Specifies a desired timeout relative to the current time. The event loop
-   * will subtract the then-current time to determine the correct relative
-   * timeout value.
-   * @param secs Seconds
-   * @param msecs Milleseconds
-   */
+    /**
+     * @return True if a timeout value is set, regardless of whether
+     * the time has expired or not.
+     */
     bool Set();
+    /**
+     * @return True if a timeout value is set and has expired.
+     */
     bool Expired();
     struct timespec when;
 };
