06d51b92e6f9b747354393df01e788be65f08128
Norton Allen <allen@huarp.harvard.edu>
2020-02-08 15:14:40 -0500

    flight: install setup/reset scripts

--- utils/flight/dasng_setup.sh
+++ utils/flight/dasng_setup.sh
@@ -4,6 +4,10 @@
 # It should live in /usr/local/share/linkeng/setup
 # Obviously, it should be run *after* the basic le-dasng installation
 
+# Make sure we are running as root
+if [ `id -u` -ne 0 ]; then
+  exec sudo $0 $*
+fi
 
 # Check if flight user exists and add if necessary
 if grep -q "^flight:" /etc/passwd; then
@@ -17,8 +21,10 @@ fi
 if [ ! -d /home/flight ]; then
   mkdir -p /home/flight/.ssh
   
-  if [ -f ~/.ssh/authorized_keys ]; then
-    cp ~/.ssh/authorized_keys /home/flight/.ssh/
+  echo "SUDO_USER = $SUDO_USER"
+  if [ -f ~$SUDO_USER/.ssh/authorized_keys ]; then
+    echo "Copying ssh keys from ~$SUDO_USER/.ssh"
+    cp ~$SUDO_USER/.ssh/authorized_keys /home/flight/.ssh/
   else
     echo "dasng_setup.sh: WARNING: authorized_keys file not found"
   fi
@@ -33,6 +39,8 @@ service=/usr/local/share/linkeng/setup/dasng.service
 if [ -f $service ]; then
   cp $service /lib/systemd/system
   echo "dasng_setup.sh: Copied dasng.service into /lib/systemd/system"
+  systemctl enable dasng &&
+  systemctl start dasng
 else
   echo "dasng_setup.sh: Skipping systemd configuration"
 fi
