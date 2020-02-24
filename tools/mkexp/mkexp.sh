a11f61d9cf58aa29a5a60b5873f67d6ad03ba96d
miles <miles@huarp.harvard.edu>
2020-02-24 10:42:07 -0500

    mkexp: minor edits to the creation scripts.

--- tools/mkexp/mkexp.sh
+++ tools/mkexp/mkexp.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+# Make sure we are running as root
+if [ `id -u` -ne 0 ]; then
+  exec sudo $0 $*
+fi
+
+# Make sure we have an experiment name
 if [ "$1" != "" ]; then
   export Experiment=$1
   echo "Experiment named captured as [$1]"
@@ -8,10 +14,12 @@ else
   exit 1
 fi
 
-sudo mkexpdir $Experiment
+# Uncertain about invocation?
+# Create the proper directories
+mkexpdir $Experiment
+export ExpDir=/home/$Experiment
 
-export ExpDir=/home/$Experiment/src/TM
-
+# Copy all the files
 cp src/services $ExpDir/services
 cp src/base.tmc $ExpDir/base.tmc
 cp src/Experiment.config $ExpDir/Experiment.config
@@ -25,7 +33,7 @@ cp src/TMbase.genui $ExpDir/TMbase.genui
 cp src/TMbase.tbl $ExpDir/TMbase.tbl
 cp src/VERSION $ExpDir/VERSION
 
-#sudo sed 's/Demo/'$Experiment'/g' $ExpDir/*
-sudo sed -i 's/@NAME@/'$Experiment'/g' $ExpDir/*
+# Change the names accordingly
+sed -i 's/@NAME@/'$Experiment'/g' $ExpDir/*
 
 exit 0
--- tools/mkexp/mkexpdir.sh
+++ tools/mkexp/mkexpdir.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+# Make sure we are running as root
+if [ `id -u` -ne 0 ]; then
+  exec sudo $0 $*
+fi
+
+# Make sure we have an Experiment name
 if [ "$1" != "" ]; then
   export Experiment=$1
   echo "Experiment named captured as [$1]"
@@ -8,16 +14,14 @@ else
   exit 1
 fi
 
-sudo mkdir /home/$Experiment
-sudo chmod g+w /home/$Experiment
-sudo chown $USERNAME /home/$Experiment
+# Create the Experiment directory
+mkdir /home/$Experiment
+chmod g+w /home/$Experiment
+chown $USERNAME /home/$Experiment
 
-sudo mkdir /home/$Experiment/src
-sudo chmod g+w /home/$Experiment/src
-sudo chown $USERNAME /home/$Experiment/src
-
-sudo mkdir /home/$Experiment/src/TM
-sudo chmod g+w /home/$Experiment/src/TM
-sudo chown $USERNAME /home/$Experiment/src/TM
+# Create the corresponding bin directory
+mkdir /home/$Experiment/bin
+chmod g+w /home/$Experiment/bin
+chown $USERNAME /home/$Experiment/bin
 
 exit 0
