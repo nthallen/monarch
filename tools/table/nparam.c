08f052348e3608bf138070bf40b147e25b0f82e8
miles <miles@huarp.harvard.edu>
2020-04-20 14:21:21 -0400

    nctable: edits

--- tools/table/nparam.c
+++ tools/table/nparam.c
@@ -27,36 +27,41 @@ int DatumHeight(int nrows) {
 int RuleThickness = 1;
 
 void preview_window( PTG_OUTPUT_FILE f, const char *name, int w, int h ) {
-  if (do_output) window = tbl_window( name, w, h );
+  if (do_output) {
+    char* argv[2] = {"", "/dev/tty"};
+    nct_init_options(2, argv);
+    winnum = nct_init( name, w, h );
+  }
 }
 
 void preview_label( PTG_OUTPUT_FILE f, const char *str, int r, int c ) {
-  if (do_output) tbl_label( window, str, r, c );
+  if (do_output) nct_string( winnum, str, r, c );
 }
 
 void preview_field( PTG_OUTPUT_FILE f, int fldnum, int r, int c, int w, int h ) {
   if (do_output) {
     char buf[10];
     sprintf(buf, "%d", fldnum);
-    tbl_field( window, buf, r, c, w, h );
+    nct_string( winnum, buf, r, c, w, h );
   }
 }
 
 void preview_loop( PTG_OUTPUT_FILE f ) {
   if ( do_output) {
-    PtRealizeWidget(window);
-    PtMainLoop();
+    if (nct_getch == 'q') {
+      return;
+    }
   }
 }
 
 void preview_vrule( PTG_OUTPUT_FILE f, int r, int c, int h, int dbl ) {
   if ( do_output ) {
-    tbl_vert_sep( window, r, c, h, dbl );
+    nct_vrule( winnum, r, c, h, dbl );
   }
 }
 
 void preview_hrule( PTG_OUTPUT_FILE f, int r, int c, int w, int dbl ) {
   if ( do_output ) {
-    tbl_horiz_sep( window, r, c, w, dbl );
+    nct_hrule( winnum, r, c, w, dbl );
   }
 }
