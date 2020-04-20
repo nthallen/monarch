54c208ffb73a171abb64338c50c7f78f952625ea
miles <miles@huarp.harvard.edu>
2020-04-20 13:40:59 -0400

    nctable: added preview functions to nctable.ptg, nparam.c

--- tools/table/nparam.c
+++ tools/table/nparam.c
@@ -1,6 +1,7 @@
 /* nparam.c Parametrized values for ntable (the text version ) */
 #include <string.h>
 #include "param.h"
+#include "nctable.h"
 
 int ColSpace = 1;
 
@@ -24,3 +25,38 @@ int DatumHeight(int nrows) {
 }
 
 int RuleThickness = 1;
+
+void preview_window( PTG_OUTPUT_FILE f, const char *name, int w, int h ) {
+  if (do_output) window = tbl_window( name, w, h );
+}
+
+void preview_label( PTG_OUTPUT_FILE f, const char *str, int r, int c ) {
+  if (do_output) tbl_label( window, str, r, c );
+}
+
+void preview_field( PTG_OUTPUT_FILE f, int fldnum, int r, int c, int w, int h ) {
+  if (do_output) {
+    char buf[10];
+    sprintf(buf, "%d", fldnum);
+    tbl_field( window, buf, r, c, w, h );
+  }
+}
+
+void preview_loop( PTG_OUTPUT_FILE f ) {
+  if ( do_output) {
+    PtRealizeWidget(window);
+    PtMainLoop();
+  }
+}
+
+void preview_vrule( PTG_OUTPUT_FILE f, int r, int c, int h, int dbl ) {
+  if ( do_output ) {
+    tbl_vert_sep( window, r, c, h, dbl );
+  }
+}
+
+void preview_hrule( PTG_OUTPUT_FILE f, int r, int c, int w, int dbl ) {
+  if ( do_output ) {
+    tbl_horiz_sep( window, r, c, w, dbl );
+  }
+}
