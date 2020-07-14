e94908b2c70d4f88c4101f6f1e526c10f5f0308c
Norton Allen <allen@huarp.harvard.edu>
2020-07-14 15:41:09 -0400

    run_server script

---
+++ test/run_server.sh
@@ -0,0 +1,117 @@
+#! /bin/bash
+# run_server.sh
+# This script is charged with running all things on the
+# server side of this client/server test. This script is
+# designed to run on both the local machine and the remote
+# machine (when a remote machine is involved).
+#
+# ./run_server.sh local start_server_memo
+#    If FlightNode is defined, ssh to FlightNode
+#    and run ./run_server.sh start_server_memo
+# ./run_server.sh start_server_memo
+#    Start memo
+# ./run_server.sh local start_server
+#    If FlightNode is defined, ssh to FlightNode
+#    and run ./run_server.sh start_server
+#    Otherwise, start the server as if local were not defined
+# ./run_server.sh start_server "Run Description"
+#    Run the server, then monitor netstat -na | grep :3500 for
+#    TIME_WAIT, reporting an error if it is present and waiting
+#    until it is clear before ending.
+# ./run_server.sh local stop_server_memo
+#    Same as start_server_memo but for stopping
+#
+# In all non-local cases, this script must establish the
+# environment by setting the PATH and cd-ing to the appropriate
+# run directory. Additional configuration values will be read
+# from Experiment.config
+#
+# On the remote machine, Experiment.config and this script must be
+# copied into the login directory, and HomeDir should point to the
+# location where source code is. The actual server executable is
+# located in another directory that should be specified with a
+# nonstandard BINDIR value.
+#   HomeDir should apply to the source directory on remote node.
+#   BINDIR ideally should work for either node.
+
+function nl_error {
+  echo "run_server: [ERROR] $*" >&2
+  exit 1
+}
+
+[ -z "$1" ] && nl_error "Must specify args"
+
+export PATH=/bin:/usr/bin:/usr/local/bin
+. /usr/local/libexec/load_exconfig.sh
+[ -n "$CFG_ERROR" ] && nl_error "$CFG_ERROR"
+[ -n "$BINDIR" ] && PATH=$PATH:$BINDIR
+if [ "$1" != "local" ]; then
+  cd $HomeDir
+fi
+
+if [ "$1" = "local" ]; then
+  shift 1
+  if [ -n "$FlightNode" ]; then
+    case "$1" in
+      start_server)
+    ssh -t $FlightNode $HomeDir/run_server.sh $* 2>&1 | tee start_server.tmp.log
+    sed -n -e 's/\r//' -e '/ERROR\|WARNING/ s/^[^\[]*//p' start_server.tmp.log |
+      while read errline; do
+        msg -n rmt "$errline"
+      done
+    mv start_server.tmp.log start_server.log
+    exit 0
+  else
+    case "$1" in
+      start_server_memo) exit 0;;
+      start_server) ;;
+      stop_server_memo) exit 0;;
+      *) nl_error "Invalid command: '$1'";;
+    esac
+    # Fall through to run test below
+  fi
+fi
+
+# Here we are either running on the remote server
+# or there is no remote server. We should not get
+# here on the local server if the command is start_server_*
+
+[ "$1" != "start_server" -a -z "$FlightNode" ] && nl_error "server_memo command in local mode"
+case "$1" in
+  start_server_memo)
+    memo -o run_server.log -V
+    exit 0;;
+  stop_server_memo)
+    clean_runfiles -k
+    exit 0;;
+  start_server)
+    ;;
+  *) nl_error "Invalid command #2: '$1'";;
+esac
+shift 1
+srvrmode=-mV
+[ -z "$FlightNode" ] && srvrmode="$srvrmode -sUnix"
+msg start_server $*
+test_server $srvrmode
+TW=`$ns -na | grep ':3500 .* TIME_WAIT' | sed -e 's/\r//'`
+if [ -n "$TW" ]; then
+  msg "[ERROR] $TW"
+  TWcount=0
+  while [ -n "$TW" ]; do
+    TW=`$ns -na | grep ':3500 .* TIME_WAIT'`
+    if [ -n "$TW" ]; then
+      if [ $TWcount -lt 30 ]; then
+        let TWcount=TWcount+1
+      else
+        msg "Still waiting on TIME_WAIT"
+        TWcount=0
+      fi
+      sleep 1
+    fi
+  done
+  msg "TIME_WAIT cleared"
+fi
+# Create start_server.log to signal run_test that server
+# has terminated, but only in the local case.
+# This file needs to be created on the local machine.
+[ -z "$FlightNode" ] && touch start_server.log
--- test/run_test.sh
+++ test/run_test.sh
@@ -5,16 +5,9 @@ iterations=2
 scenarios="11 12 13"
 srvrname=127.0.0.1
 srvrmode=''
-cltlocal=''
 export PATH=$PATH:/cygdrive/c/WINDOWS/system32
 ns=netstat
-if [ -n "$srvrname" ]; then
-  cltlocal="-t $srvrname"
-  srvrmode='-s TCP'
-else
-  srvrmode='-s Unix'
-fi
-  
+
 function hit_any_key {
   if [ $pause = yes ]; then
     echo Hit any key to continue:
@@ -26,7 +19,6 @@ echo "\$0 is '$0'"
 if [ -z "$STY" ]; then
   . /usr/local/libexec/load_exconfig.sh
   [ -n "$CFG_ERROR" ] && exit 0
-  export TMBINDIR=.
   export BINDIR=../../build-quit_change/test
   screen $0
   clear # after exiting screen
@@ -37,59 +29,53 @@ else
   echo "BINDIR is '$BINDIR'"
   # echo "PATH is:"
   # echo $PATH | sed -e 's/:/\n  /g' -e 's/^/  /'
+  cltlocal=''
+  [ -n "$FlightNode" ] && cltlocal="-t $FlightNode"
+
   clean_runfiles
   hit_any_key
   
   screen -t memo memo -o TEST.log -V -l0
   screen -X select 0
-  echo "memo has been started"
   waitfor /var/run/linkeng/$Experiment/memo 15
+  echo "local memo has been started"
+  
+  screen -t rmemo ./run_server.sh local start_server_memo
+  sleep 2
+  screen -X select 0
+  echo "remote memo has been started"
   hit_any_key
 
   count=0  
   while [ $count -lt $iterations ]; do
     let count=count+1
     for scenario in $scenarios; do #
-      echo "Iteration $count Scenario $scenario"
-      msg "Iteration $count Scenario $scenario"
+      desc="Iteration $count Scenario $scenario"
+      echo $desc
       # Now we can loop through tests
-      screen -t server $BINDIR/test_server $srvrmode
-      screen -X select 0
+      rm -f start_server.tmp.log start_server.log
+      screen -t server ./run_server.sh local start_server "$desc"
+      # screen -t server $BINDIR/test_server $srvrmode
       if [ -z "$srvrname" ]; then
+        screen -X select 0
         echo "server has been started, will wait for name"
         waitfor /var/run/linkeng/$Experiment/test 5
       else
-        # echo "server has been started"
-        sleep 1
+        sleep 5
+        screen -X select 0
+        echo "Remote server has been started"
       fi
       hit_any_key
       
       $BINDIR/test_client -S $scenario $cltlocal
       # now check on the status. Has the server terminated?
       clean_runfiles >/dev/null
-      if [ -z "$srvrname" -a -e /var/run/linkeng/$Experiment/test ]; then
-        msg "[ERROR] Server has not terminated"
-        kill -9 `cat /var/run/linkeng/$Experiment/test.pid`
-      fi
-      
-      if [ -n "$srvrname" ]; then
-        TW=`$ns -na | grep ':3500 .* TIME_WAIT' | sed -e 's/\r//'`
-        if [ -n "$TW" ]; then
-          msg "[ERROR] $TW"
-          TWcount=0
-          while [ -n "$TW" ]; do
-            TW=`$ns -na | grep ':3500 .* TIME_WAIT'`
-            if [ -n "$TW" ]; then
-              if [ $TWcount -lt 30 ]; then
-                let TWcount=TWcount+1
-              else
-                msg "Still waiting on TIME_WAIT"
-                TWcount=0
-              fi
-              sleep 1
-            fi
-          done
-          msg "TIME_WAIT cleared"
+      if [ ! -f start_server.log ]; then
+        if [ -f start_server.tmp.log ]; then
+          msg "[ERROR] Server has not terminated"
+          waitfor -q start_server.log forever
+        else
+          msg "[ERROR] 'No start_server*.log found'"
         fi
       fi
       
@@ -98,6 +84,7 @@ else
   done
   
   # Shut down memo (crudely)
+  ./run_server.sh local stop_server_memo
   clean_runfiles -k
   hit_any_key
 fi
