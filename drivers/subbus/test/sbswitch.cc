15b891d544b626becfdc5a92b2ff30c52cee7186
Norton Allen <allen@huarp.harvard.edu>
2021-04-24 14:59:01 -0400

    More operating modes for sbswitch

--- drivers/subbus/test/sbswitch.cc
+++ drivers/subbus/test/sbswitch.cc
@@ -9,16 +9,35 @@ const char *subbus_svc = "subbus";
 const char *subbus_subsvc = "serusb";
 const char *subbus_addr = 0;
 const char *switch_mask = 0;
+const char *switch_invert = 0;
+bool quit_if_nonzero = false;
+
+void sbswitch_args(const char *arg) {
+  if (switch_invert) {
+    msg(3, "Expected no more than two parameters");
+  } else if (switch_mask) {
+    switch_invert = arg;
+  } else {
+    switch_mask = arg;
+  }
+}
 
 int main( int argc, char **argv ) {
   oui_init_options(argc, argv);
-  uint16_t sw;
+  uint16_t sw = 0xFF;
   uint16_t mask = 0xF;
   uint16_t addr = 0x7;
+  uint16_t invert = 0;
+  int rv = quit_if_nonzero ? 1 : 0; // default to flight mode
 
   if ( switch_mask ) {
     unsigned long tmp_mask = strtoul(switch_mask,NULL,0);
     mask = (uint16_t)tmp_mask;
+  }
+
+  if ( switch_invert ) {
+    unsigned long tmp_invert = strtoul(switch_invert,NULL,0);
+    invert = (uint16_t)tmp_invert;
   }
   
   if (subbus_addr) {
@@ -28,10 +47,17 @@ int main( int argc, char **argv ) {
 
   subbuspp *SB = new subbuspp(subbus_svc, subbus_subsvc);
   if (SB->load()) {
-    sw = ((SB->read_subbus(addr) & mask) | ~mask) & 0xFF;
-    printf( "%02X\n", sw );
+    sw = (((SB->read_subbus(addr) & mask) | ~mask) ^ invert) & 0xFF;
   } else {
-    msg( 3, "Unable to load %s/%s", subbus_svc, subbus_subsvc);
+    msg(2, "Unable to load %s/%s", subbus_svc, subbus_subsvc);
   }
-  return 0;
+  if (quit_if_nonzero) {
+    if (sw != 0xFF ) {
+      SB->subbus_quit();
+      rv = 0; // not flight mode
+    }
+  } else {
+    printf( "%02X\n", sw );
+  }
+  return rv;
 }
--- drivers/subbus/test/sbswitch.h
+++ drivers/subbus/test/sbswitch.h
@@ -5,5 +5,7 @@ extern const char *subbus_svc;
 extern const char *subbus_subsvc;
 extern const char *subbus_addr;
 extern const char *switch_mask;
+extern bool quit_if_nonzero;
+void sbswitch_args(const char *arg);
 
 #endif
