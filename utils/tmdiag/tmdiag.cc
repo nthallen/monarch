6cfdf4edcb9fd74a316f0258bfdc33b79b07e51d
Norton Allen <allen@huarp.harvard.edu>
2022-11-22 10:59:37 -0500

    tmdiag work in progress

--- utils/tmdiag/tmdiag.cc
+++ utils/tmdiag/tmdiag.cc
@@ -2,14 +2,18 @@
  */
 #include <errno.h>
 #include <string.h>
+#include <unistd.h> // for optind, opterr
 #include "tmdiag.h"
 #include "dasio/msg.h"
+#include "dasio/tm.h"
 #include "oui.h"
 
 tmdiag tmd;
 
 int main(int argc, char **argv) {
   oui_init_options(argc, argv);
+  load_tmdac(".");
+  tmd.process_args(argc, argv);
 }
 
 tmdiag::tmdiag() :
@@ -18,6 +22,25 @@ tmdiag::tmdiag() :
     bufsize(20000),
     buf(0) {
   buf = (char*)new_memory(bufsize);
+}
+
+void tmdiag::process_args(int argc, char **argv) {
+  int optltr;
+
+  optind = OPTIND_RESET;
+  opterr = 0;
+  while ((optltr = getopt(argc, argv, opt_string)) != -1) {
+    switch (optltr) {
+      case '?':
+        msg(3, "Unrecognized Option -%c", optopt);
+      default:
+        break;
+    }
+  }
+  for (; optind < argc; optind++) {
+    optarg = argv[optind];
+    tmd.process_file(optarg);
+  }
 }
 
 void tmdiag::process_file(const char *filename) {
@@ -44,15 +67,15 @@ void tmdiag::process_file(const char *filename) {
   if (check_logic_error(rc != fsize, "wrong read length from fread()"))
     return;
   for (offset = 0; offset < fsize;) {
-    int rc = process_record(&buf[offset]);
-    if (rc < 0) return; // error already processed
-    offset += rc;
+    if (process_record(&buf[offset]))
+      break;
   }
-  fclose(curfp);
-  curfp = 0;
+  cleanup_error();
 }
 
 void tmdiag::cleanup_error() {
+  // If additional error logic is added here, create a non-error
+  // cleanup function that can be called from process_file().
   if (curfp) {
     fclose(curfp);
     curfp = 0;
@@ -70,15 +93,63 @@ bool tmdiag::check_file_error(bool val, const char *where) {
 
 bool tmdiag::check_logic_error(bool val, const char *where) {
   if (val) {
-    msg(MSG_ERROR, "%s: %s", curfile, where);
+    if (where) msg(MSG_ERROR, "%s: %s", curfile, where);
     cleanup_error();
   }
   return val;
 }
 
-int tmdiag::process_record(const char *rec) {
+bool tmdiag::process_record(const char *rawrec) {
   if (check_logic_error(fsize-offset < sizeof(tm_hdr_t), "Short record at EOF"))
-    return -1;
-  struct tm_msg *tms = (struct tm_msg *)rec;
-  return -1;
+    return true;
+  struct tm_msg *rec = (struct tm_msg *)rawrec;
+  if (check_logic_error(rec->hdr.tm_id != TMHDR_WORD, 0)) {
+    msg(MSG_ERROR, "%s: Invalid message header at offset %d",
+          curfile, offset);
+    return true;
+  }
+  switch (rec->hdr.tm_type) {
+    case TMTYPE_INIT: // 0x0100
+      return (process_init(rec));
+    case TMTYPE_TSTAMP: // 0x0200
+      return (process_tstamp(rec));
+    case TMTYPE_DATA_T3: // 0x0303
+      return (process_data_t3(rec));
+    case TMTYPE_QUIT: // 0x0400
+      return (process_quit(rec));
+    case TMTYPE_DATA_T1: // 0x0301
+    case TMTYPE_DATA_T2: // 0x0302
+    case TMTYPE_DATA_T4: // 0x0304
+    case TMTYPE_ACK_T3: // 0x0503: Never referenced or documented
+    default:
+      msg(MSG_ERROR, "%s: Unsupported record type 0x%04X at offset %d",
+        curfile, rec->hdr.tm_type, offset);
+      cleanup_error();
+      return true;
+  }
+}
+
+bool tmdiag::process_init(struct tm_msg *rec) {
+  tm_info_t *tmi = &rec->body.init;
+  tm_dac_t *tmdac = &tmi->tm;
+  // Compare tmdac to tm_info.tm
+  //   Compare tmdac->tmid.version
+  //   Compare tmdac->tmid.md5
+  //   Compare tmdac->nbminf
+  if (check_file_error(offset+sizeof(tm_hdr_t)+sizeof(tm_info_t) > fsize,
+      "Incomplete TM_INIT record"))
+    return true;
+  return false;
+}
+
+bool tmdiag::process_tstamp(struct tm_msg *rec) {
+  return true;
+}
+
+bool tmdiag::process_data_t3(struct tm_msg *rec) {
+  return true;
+}
+
+bool tmdiag::process_quit(struct tm_msg *rec) {
+  return true;
 }
--- utils/tmdiag/tmdiag.h
+++ utils/tmdiag/tmdiag.h
@@ -14,15 +14,37 @@ class tmdiag {
   public:
     tmdiag();
     void process_file(const char *filename);
+    void process_args(int argc, char **argv);
     /**
-     * @return record size in bytes or -1 on error
-     * If any error occurs, the file will be closed and a
-     * message reported before returning.
+     * @param rec pointer to the current record in buf
+     * @return true if there was an error
+     * On success, offset will be updated to the next
+     * record. On error, the file will be closed and
+     * a message reported before returning.
      */
-    int process_record(const char *rec);
+    bool process_record(const char *rec);
+    /**
+     * @param msg pointer to the current tm_msg
+     * @return true if there was an error
+     * On success, offset will be updated to the next
+     * record. On error, the file will be closed and
+     * a message reported before returning.
+     */
+    bool process_init(struct tm_msg *msg);
+    bool process_tstamp(struct tm_msg *msg);
+    bool process_data_t3(struct tm_msg *msg);
+    bool process_quit(struct tm_msg *msg);
   protected:
     bool check_file_error(bool val, const char *where);
+    /**
+     * @param val If true, invoke cleanup_error()
+     * @param where If non-zero, display error message when val is true
+     * @return val for convenience
+     */
     bool check_logic_error(bool val, const char *where);
+    /**
+     * Currently just closes the file.
+     */
     void cleanup_error();
     const char *curfile;
     FILE *curfp;
