54611c11cbad4ebefb373ff192031ec9d2853f37
Norton Allen <allen@huarp.harvard.edu>
2023-02-22 12:11:34 -0500

    ssh-agent: suppress unimportant error

--- tools/setup/ssh_agent.sh
+++ tools/setup/ssh_agent.sh
@@ -4,7 +4,7 @@ SSH_ENV="$HOME/.ssh/environment"
 
 function start_agent {
      echo "Initialising new SSH agent..."
-     rm -rf /tmp/ssh-*
+     rm -rf /tmp/ssh-* 2> /dev/null
      /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
      echo succeeded
      chmod 600 "${SSH_ENV}"
