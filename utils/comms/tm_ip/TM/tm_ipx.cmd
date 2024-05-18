42c0f03c99a144b6937ee25e77ed63010d184cf1
Norton Allen <allen@huarp.harvard.edu>
2024-05-18 11:15:04 -0600

    tm_ipx: Bad msg

--- utils/comms/tm_ip/TM/tm_ipx.cmd
+++ utils/comms/tm_ip/TM/tm_ipx.cmd
@@ -3,7 +3,7 @@
 &^command
   : TM_Export set UDP MTU %d * {
       if ($5 <= 20) {
-        msg(MSG_ERROR, "Request MTU (%d) too small");
+        msg(MSG_ERROR, "Request MTU (%d) too small", $5);
       } else {
         if_TMIPX.Turf("M%d\n", $5);
       }
