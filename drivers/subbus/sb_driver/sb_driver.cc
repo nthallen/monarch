b9f9463d944f2b20f6251b67c8113c53949dbabf
Norton Allen <allen@huarp.harvard.edu>
2024-07-12 15:17:22 -0400

    sb_driver take over init() to load, delegate setup()

--- drivers/subbus/sb_driver/sb_driver.cc
+++ drivers/subbus/sb_driver/sb_driver.cc
@@ -2,5 +2,18 @@
 #include "sb_driver.h"
 
 SB_driver::SB_driver(const char *service, const char *subservice)
-    : subbuspp(service, subservice) {
+    : subbuspp(service, subservice)
+{
+}
+
+void SB_driver::init()
+{
+  if (load()) {
+    msg(MSG, "%s: connected to %s", iname, get_subbus_name());
+  }
+  setup();
+}
+
+void SB_driver::setup()
+{
 }
--- drivers/subbus/sb_driver/sb_driver.h
+++ drivers/subbus/sb_driver/sb_driver.h
@@ -3,5 +3,7 @@
 class SB_driver : public subbuspp {
   public:
     SB_driver(const char *service, const char *subservice);
+    void init();
+    virtual void setup();
   protected:
 };
